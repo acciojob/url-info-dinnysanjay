622a68fd0e4b5f998107879d875b18fcab6c34b2
dinnysanjay <160900894+dinnysanjay@users.noreply.github.com>
2024-04-21 10:50:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 //your JS code here. If required.
+var url = window.location.href; // This will get the current URL
+var length = url.length; // This will get the length of the URL
+alert("The length of the URL is: " + length); 
